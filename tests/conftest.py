232c22ade47f1d92177c4a2a5384356dafd30ba2
Vaibhav Jain <vajain@redhat.com>
2023-11-06 11:54:18 +0530

    Implement HTTP client

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,11 @@ from pathlib import Path
 from typing import Callable, TypeVar
 
 import pytest
-from caikit_nlp_client.grpc_channel import GrpcChannelConfig, make_channel
+import requests
 from grpc_health.v1 import health_pb2, health_pb2_grpc
+
+from src.caikit_nlp_client.grpc_channel import GrpcChannelConfig, make_channel
+from src.caikit_nlp_client.http_config import HTTPConfig
 
 _T = TypeVar("_T")
 
@@ -46,6 +49,7 @@ def caikit_nlp_runtime(monkeypatch):
         "RUNTIME_LOCAL_MODELS_DIR",
         str(Path(__file__).parent / "tiny_models"),
     )
+    monkeypatch.setenv("RUNTIME_HTTP_PORT", "8080")
     # TODO: add port configuration here using `RUNTIME_GRPC_PORT`
 
 
@@ -90,3 +94,35 @@ def grpc_server(caikit_nlp_runtime, grpc_server_port):
     yield grpc_server
 
     grpc_server.stop()
+
+
+@pytest.fixture
+def http_config(caikit_nlp_runtime):
+    from caikit.config import get_config
+
+    return HTTPConfig(
+        host="localhost",
+        port=get_config().runtime.http.port,
+        tls=False,
+    )
+
+
+@pytest.fixture
+def http_server(caikit_nlp_runtime, http_config):
+    from caikit.runtime.http_server import RuntimeHTTPServer
+
+    http_server = RuntimeHTTPServer()
+    http_server.start(blocking=False)
+
+    def health_check():
+        response = requests.get(
+            f"http://{http_config.host}:{http_config.port}/health",
+        )
+        assert response.status_code == 200
+        assert response.text == "OK"
+
+    wait_until(health_check, timeout=30, pause=0.5)
+
+    yield http_server
+
+    http_server.stop()
--- tests/test_http_predict.py
+++ tests/test_http_predict.py
@@ -1,17 +1,49 @@
 import pytest
-import requests
+
+from src.caikit_nlp_client.http_client import HTTPCaikitNlpClient
 
 
-@pytest.mark.xfail(reason="Stub test")
-def test_http_endpoint(model_name):
-    api_url = f"http://localhost:8080/api/v1/{model_name}/task/text-generation"
+@pytest.fixture
+def http_client(http_config, http_server) -> HTTPCaikitNlpClient:
+    """Returns a grpc client connected to a locally running server"""
+    return HTTPCaikitNlpClient(http_config)
 
-    json_input = {
-        "inputs": "At what temperature does liquid Nitrogen boil?",
-        "parameters": {"max_new_tokens": 200, "min_new_tokens": 10},
-    }
-    response = requests.post(
-        api_url,
-        json=json_input,
+
+def test_generate_text(http_client, model_name):
+    response = http_client.generate_text(model_name, "What does foobar mean?")
+    assert response
+
+
+def test_generate_text_with_optional_args(http_client, model_name):
+    response = http_client.generate_text(
+        model_name,
+        "What does foobar mean?",
+        max_new_tokens=20,
+        min_new_tokens=4,
     )
-    assert response.status_code == 200
+    assert response
+
+
+def test_generate_text_with_no_model_id(http_client):
+    with pytest.raises(ValueError, match="request must have a model id"):
+        http_client.generate_text("", "What does foobar mean?")
+
+
+@pytest.mark.skip(reason="HTTP stream is not working as expected")
+def test_generate_text_stream(http_client, model_name):
+    results = http_client.generate_text_stream(
+        model_name, "What is the meaning of life?"
+    )
+    assert len(results) == 9
+
+
+@pytest.mark.skip(reason="HTTP stream is not working as expected")
+def test_generate_text_stream_with_optional_args(http_client, model_name):
+    results = http_client.generate_text_stream(
+        model_name,
+        "What is the meaning of life?",
+        preserve_input_text=False,
+        max_new_tokens=20,
+        min_new_tokens=4,
+    )
+    assert len(results) == 9
