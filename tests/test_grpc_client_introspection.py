5623ba1dc7931af86a0ed702c88ac028763a83d6
Laurentiu Bradin <109964136+z103cb@users.noreply.github.com>
2023-11-07 13:59:38 +0200

    test: :art: Parametrized the text generation tests in the grpc client.
    Added test parametrization for the text generation endpoints

--- tests/test_grpc_client_introspection.py
+++ tests/test_grpc_client_introspection.py
@@ -1,4 +1,7 @@
+from dataclasses import dataclass
+
 import pytest
+
 from caikit_nlp_client.grpc_client_introspection import GrpcCaikitNlpClientIntrospection
 
 
@@ -8,23 +11,66 @@ def connected_client(channel):
     return GrpcCaikitNlpClientIntrospection(channel)
 
 
-def test_generate_text(model_name, connected_client):
+@dataclass
+class GenerateTextTestData:
+    model_id: str
+    input_text: str
+    expected_text: str
+    preserve_input_text: bool
+    max_new_tokens: int
+    min_new_tokens: int
+
+
+TEST_CASES: list[GenerateTextTestData] = [
+    GenerateTextTestData(
+        "BertForSequenceClassification-caikit",
+        "Hello, my dog is cute",
+        "hello, my dog is cuteρ 白ᅩ xʒ ハ ܬ 河 水ʒ セタ x x セ v セ 河 興 セ",
+        True,
+        20,
+        5,
+    ),
+    GenerateTextTestData(
+        "BloomForCausalLM-caikit",
+        "Hello stub",
+        "Hello stububububububububububububububububububububub",
+        True,
+        20,
+        5,
+    ),
+    GenerateTextTestData(
+        "T5ForConditionalGeneration-caikit",
+        "Hello, Mr. Bond!",
+        "",
+        True,
+        20,
+        5,
+    ),
+]
+
+STREAMING_TEST_CASES: list[GenerateTextTestData] = []
+
+
+@pytest.mark.parametrize("test_case", TEST_CASES)
+def test_generate_text(connected_client, test_case):
     generated_text = connected_client.generate_text(
-        model_name, "What does foobar mean?"
+        test_case.model_id,
+        test_case.input_text,
     )
 
-    assert generated_text
+    assert generated_text == test_case.expected_text
 
 
-def test_generate_text_with_optional_args(connected_client, model_name):
+@pytest.mark.parametrize("test_case", TEST_CASES)
+def test_generate_text_with_optional_args(connected_client, test_case):
     generated_text = connected_client.generate_text(
-        model_name,
-        "What does foobar mean?",
-        preserve_input_text=False,
-        max_new_tokens=20,
-        min_new_tokens=4,
+        test_case.model_id,
+        test_case.input_text,
+        preserve_input_text=test_case.preserve_input_text,
+        max_new_tokens=test_case.max_new_tokens,
+        min_new_tokens=test_case.min_new_tokens,
     )
-    assert generated_text
+    assert generated_text == test_case.expected_text
 
 
 def test_generate_text_with_no_model_id(connected_client):
@@ -32,25 +78,24 @@ def test_generate_text_with_no_model_id(connected_client):
         connected_client.generate_text("", "What does foobar mean?")
 
 
-@pytest.mark.xfail(
-    reason="BertForSequenceClassification-caikit does not support streaming"
-)
-def test_generate_text_stream(model_name, connected_client):
+# @pytest.mark.parametrize("test_case", STREAMING_TEST_CASES)
+@pytest.mark.xfail(reason="None of the models defined support streaming")
+def test_generate_text_stream(connected_client, test_case):
     results = connected_client.generate_text_stream(
-        model_name, "What is the meaning of life?"
+        test_case.model_id,
+        test_case.input_text,
     )
     assert results
 
 
-@pytest.mark.xfail(
-    reason="BertForSequenceClassification-caikit does not support streaming"
-)
-def test_generate_text_stream_with_optional_args(model_name, connected_client):
+# @pytest.mark.parametrize("test_case", STREAMING_TEST_CASES)
+@pytest.mark.xfail(reason="None of the models defined support streaming")
+def test_generate_text_stream_with_optional_args(connected_client, test_case):
     results = connected_client.generate_text_stream(
-        model_name,
-        "What is the meaning of life?",
-        preserve_input_text=False,
-        max_new_tokens=20,
-        min_new_tokens=4,
+        test_case.model_id,
+        test_case.input_text,
+        preserve_input_text=test_case.preserve_input_text,
+        max_new_tokens=test_case.max_new_tokens,
+        min_new_tokens=test_case.min_new_tokens,
     )
     assert results
