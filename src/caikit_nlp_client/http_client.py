d9abe56cf3b458fc9bcd2d5c74bf567c741897ad
Laurentiu Bradin <109964136+z103cb@users.noreply.github.com>
2023-11-09 14:51:52 +0200

    http client: additional asserts for mypy

--- src/caikit_nlp_client/http_client.py
+++ src/caikit_nlp_client/http_client.py
@@ -70,6 +70,9 @@ class HTTPCaikitNlpClient:
             log.info(f"Calling generate_text for '{model_id}'")
             json_input = create_json_request(model_id, text, **kwargs)
             if self.config.tls:
+                assert self.config.client_key_path
+                assert self.config.client_crt_path
+
                 response = requests.post(
                     self.api_url,
                     json=json_input,
