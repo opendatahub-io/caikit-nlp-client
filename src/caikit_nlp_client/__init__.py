aee9b5895f160efd71e2f6f508bc0868a0e36b27
Daniele Trifirò <dtrifiro@redhat.com>
2023-11-10 18:04:24 +0100

    refactor

--- src/caikit_nlp_client/__init__.py
+++ src/caikit_nlp_client/__init__.py
@@ -1,8 +1,7 @@
 # noqa: F401
-from .grpc_channel import GrpcChannelConfig as GRPCConfig
-from .grpc_channel import make_channel as make_grpc_channel
-from .grpc_client_introspection import GrpcCaikitNlpClientIntrospection as GRPCClient
-from .http_client import HTTPCaikitNlpClient as HTTPClient
-from .http_client import HTTPConfig  # noqa: F401
+from .grpc_client_introspection import GrpcCaikitNlpClientIntrospection as GrpcClient
+from .grpc_client_introspection import GrpcConfig
+from .http_client import HTTPCaikitNlpClient as HttpClient
+from .http_client import HTTPConfig as HttpConfig
 
-__all__ = ["GRPCConfig", "make_grpc_channel", "GRPCClient", "HTTPClient", "HTTPConfig"]
+__all__ = ["GrpcConfig" "GrpcClient", "HttpClient", "HttpConfig"]
--- src/caikit_nlp_client/grpc_client_introspection.py
+++ src/caikit_nlp_client/grpc_client_introspection.py
@@ -1,4 +1,6 @@
 import logging
+from dataclasses import dataclass
+from typing import Optional
 
 import grpc
 from google.protobuf.descriptor_pool import DescriptorPool
@@ -10,6 +12,43 @@ from grpc_reflection.v1alpha.proto_reflection_descriptor_database import (
 log = logging.getLogger(__name__)
 
 
+@dataclass
+class GrpcConfig:
+    host: str
+    port: int
+    insecure: bool = False
+    ca_cert: Optional[bytes] = None
+    client_key: Optional[bytes] = None
+    client_cert: Optional[bytes] = None
+
+
+def make_channel(config: GrpcConfig) -> grpc.Channel:
+    log.debug(f"Making a channel from this config {config}")
+    if config.host.strip() == "":
+        raise ValueError("A non empty host name is required")
+    if config.port <= 0:
+        raise ValueError("A non zero port is required")
+
+    connection = f"{config.host}:{config.port}"
+
+    if config.insecure:
+        return grpc.insecure_channel(connection)
+
+    if config.client_key is None:
+        raise ValueError("A client key is required")
+    if config.client_cert is None:
+        raise ValueError("A client cert is required")
+    if config.ca_cert is None:
+        raise ValueError("A certificate authority certificate is required")
+
+    return grpc.secure_channel(
+        connection,
+        grpc.ssl_channel_credentials(
+            config.ca_cert, config.client_key, config.client_cert
+        ),
+    )
+
+
 class GrpcCaikitNlpClientIntrospection:
     """GRPC client for a caikit nlp runtime server
 
@@ -17,7 +56,10 @@ class GrpcCaikitNlpClientIntrospection:
         channel (grpc.Channel): a connected GRPC channel for use of making the calls.
     """
 
-    def __init__(self, channel: grpc.Channel) -> None:
+    def __init__(self, config: GrpcConfig) -> None:
+        """Returns a GrpcClient to the given GRPC server"""
+
+        channel = make_channel(config)
         try:
             self.reflection_db = ProtoReflectionDescriptorDatabase(channel)
             self.desc_pool = DescriptorPool(self.reflection_db)
