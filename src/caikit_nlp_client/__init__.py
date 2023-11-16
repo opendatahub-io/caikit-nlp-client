b607c6144f88981d897522156f5444531ba1dc83
Daniele Trifirò <dtrifiro@redhat.com>
2023-11-16 11:56:08 +0100

    bump setuptools, add _version module

--- src/caikit_nlp_client/__init__.py
+++ src/caikit_nlp_client/__init__.py
@@ -1,4 +1,9 @@
 from .grpc_client import GrpcClient
 from .http_client import HttpClient
 
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
+
 __all__ = ["GrpcClient", "HttpClient"]
